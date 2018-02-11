a283e1d6d23559a9d6663ac1c83cb350225438c5
cosmosromantica <mycatshoegazer@outlook.com>
2018-02-11 17:41:29 +0300

    Implemented printing sensor values function.

--- fanboost.sh
+++ fanboost.sh
@@ -2,16 +2,25 @@
 
 
 CORE_SENSOR="/sys/devices/platform/coretemp.0/hwmon/hwmon1/temp1_input"
-FAN_PATH="/sys/devices/platform/applesmc.768/"
+FAN_PATH="/sys/devices/platform/applesmc.768"
 
-FAN_1_MANUAL="$FAN_PATH/fan1_manual"
-FAN_1_CURRENT_SPEED="$FAN_PATH/fan1_input"
-FAN_1_SPEED_SET="$FAN_PATH/fan1_output"
+FAN_1="$FAN_PATH/fan1_"
+FAN_2="$FAN_PATH/fan2_"
 
-FAN_2_MANUAL="$FAN_PATH/fan2_manual"
-FAN_2_CURRENT_SPEED="$FAN_PATH/fan2_input"
-FAN_2_SPEED_SET="$FAN_PATH/fan2_output"
+FAN_CURRENT_SPEED_PORT="input"
+FAN_SPEED_SET_PORT="output"
+FAN_MANUAL_MODE_PORT="manual"
 
 function print_sensors() {
     CORE_TEMP=$(<$CORE_SENSOR)
+    CORE_TEMP=$((CORE_TEMP / 1000))
+
+    FAN_1_RPM=$(<$FAN_1$FAN_CURRENT_SPEED_PORT)
+    FAN_2_RPM=$(<$FAN_2$FAN_CURRENT_SPEED_PORT)
+
+    printf '%s\t%s%c\n' "Core temp:" "$CORE_TEMP" "c"
+    printf '%s\t\t%s%s\n' "Fan 1:" "$FAN_1_RPM" "rpm"
+    printf '%s\t\t%s%s\n' "Fan 2:" "$FAN_2_RPM" "rpm"
 }
+
+print_sensors
